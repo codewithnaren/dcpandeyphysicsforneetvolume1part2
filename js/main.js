22d30c05860e18ac279f7d1bf7bcd685fe27e235
codewithnaren <91406174+codewithnaren@users.noreply.github.com>
2021-10-17 16:29:34 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const url = 'https://codewithnaren.github.io/socialandpoliticallife1/Polity6.pdf';
+const url = 'https://codewithnaren.github.io/dcpandeyphysicsforneetvolume1part2/dcpandeyphysicsforneetvolume1part2.pdf';
 
 
 let pdfDoc = null,
